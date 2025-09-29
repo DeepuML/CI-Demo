83e6249a2ebc1612b40703a607ef62a85866fce9
DeepuML <mahakal123321@gmail.com>
2025-09-29 23:10:34 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
 
+def subtract(a, b):
+    return a - b
 
 
-def subtract(a,b):
-    return a-b
+def multiply(a, b):
+    return a * b
 
 
-def multiply(a,b):
-    return a*b
-
-
-def divide(a,b):
+def divide(a, b):
     if b == 0:
         return "Error! Division by zero."
-    return a/b
+    return a / b
 
-result1=add(10,5)
-result2=subtract(10,5)
-result3=multiply(10,5)
-result4=divide(10,5)
+result1 = add(10, 5)
+result2 = subtract(10, 5)
+result3 = multiply(10, 5)
+result4 = divide(10, 5)
 
 print("Addition: ", result1)
 print("Subtraction: ", result2)
